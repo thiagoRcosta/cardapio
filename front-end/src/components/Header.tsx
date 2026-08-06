b4b997462f1b142471d3b8c45b1b38608581310b
thiagoRcosta <thiago.r.cst@gmail.com>
2026-08-06 19:09:00 -0300

    Criação dos componentes - Input e Header

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import logo from "../assets/cardapio-logo.png";
+
+function Header() {
+  return (
+    <div className="w-full max-w-[1280px] mx-auto flex items-center justify-between p-4 xl:py-2 xl:px-0">
+      <img src={logo} alt="Logo" width={100} />
+      <button className="bg-amber-400 p-2 w-[120px] cursor-pointer rounded-md">
+        Entrar
+      </button>
+    </div>
+  );
+}
+
+export default Header;
